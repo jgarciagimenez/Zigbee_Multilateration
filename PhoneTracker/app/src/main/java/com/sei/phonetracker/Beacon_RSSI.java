eba1748f520f7762af69522e5e2ec219caff6b2d
xismy <iye1994@gmail.com>
2017-01-18 15:48:54 +0100

    Data shipping implemented

--- PhoneTracker/app/src/main/java/com/sei/phonetracker/Beacon_RSSI.java
+++ PhoneTracker/app/src/main/java/com/sei/phonetracker/Beacon_RSSI.java
@@ -4,49 +4,86 @@ import android.content.Context;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
-import android.util.Log;
-
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by xismy on 3/01/17.
  */
 
-public class Beacon_RSSI extends AsyncTask<Object, ScanResult, Integer> {
+public class Beacon_RSSI extends AsyncTask<Object, Object, Integer> {
     private boolean measure = true;
+    private MainActivity mainActivity = null;
 
     public synchronized void stop(){
         measure = false;
     }
 
+    private ArrayList<Double> power = new ArrayList<Double>();
+
     @Override
     protected Integer doInBackground(Object... params) {
-        Context context = (Context)params[0];
+        mainActivity = (MainActivity)params[0];
+        Context context = mainActivity.getApplicationContext();
         String prefix = (String) params[1];
         WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        Socket socket;
+        PrintWriter out;
+
+        try {
+            socket = new Socket(mainActivity.IP, mainActivity.PORT);
+            out = new PrintWriter(socket.getOutputStream(), true);
+        }
+        catch (IOException exc){
+            Toast.makeText(context, "Error al crear socket", Toast.LENGTH_LONG).show();
+            return 1;
+        }
 
         while(measure){
-            wifiManager.startScan();
-            List<ScanResult> aps = wifiManager.getScanResults();
+            power = new ArrayList<Double>();
+            for(int i=0;i<5;i++) {
+                wifiManager.startScan();
+                List<ScanResult> aps = wifiManager.getScanResults();
 
-            for(ScanResult ap:aps){
-                if(ap.SSID.startsWith(prefix))
-                    publishProgress(ap);
+                for (ScanResult ap : aps) {
+                    if (ap.SSID.startsWith(prefix)) {
+                        int apIndex = Integer.parseInt(ap.SSID.substring(prefix.length()));
+                        while(power.size()<apIndex+1)
+                            power.add(0.0);
+                        power.set(apIndex, 0.2*power.get(apIndex) + 0.8*ap.level);
+                    }
+
+
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException ex) {
+
+                    }
+                }
             }
 
-            try{
-                Thread.sleep(500);
+            for(int i=0; i<power.size();i++){
+                out.println("dist "+i+" "+power.get(i)+" ");
             }
-            catch(InterruptedException ex){
-
-            }
+            out.println("loc ");
         }
+
+        try {
+            socket.close();
+        }
+        catch (IOException exc){}
         return 0;
     }
 
     @Override
-    protected void onProgressUpdate(ScanResult... value) {
+    protected void onProgressUpdate(Object... value) {
         super.onProgressUpdate(value);
-        Log.d(value[0].SSID, ""+value[0].level);
+
+        ((TextView)mainActivity.findViewById(R.id.power)).setText(value[0].toString());
     }
 }
--- PhoneTracker/app/src/main/java/com/sei/phonetracker/MainActivity.java
+++ PhoneTracker/app/src/main/java/com/sei/phonetracker/MainActivity.java
@@ -3,10 +3,14 @@ package com.sei.phonetracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
     private Beacon_RSSI rssi = null;
-    final String ap_prefix = "";
+    final String ap_prefix = "Node_MCU_";
+    final String IP = "192.168.1.1";
+    final int PORT = 65090;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (rssi == null) {
             rssi = new Beacon_RSSI();
-            rssi.execute(getApplicationContext(), ap_prefix);
+            rssi.execute(this, ap_prefix);
         }
     }
 }
